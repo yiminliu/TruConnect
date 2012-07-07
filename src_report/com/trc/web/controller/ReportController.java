523bc9167c6b559bcf7ae1983e6a8c9834a03f98
yiminliu <yimin_l@hotmail.com>
2012-07-06 17:05:40 -0700

    code cleaning

--- src_report/com/trc/web/controller/ReportController.java
+++ src_report/com/trc/web/controller/ReportController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
