939c33e457fcb1e15a93cc27de86c170aca9f619
yiminliu <yimin_l@hotmail.com>
2012-12-14 13:31:06 -0800

    replace with the code from Jon's repository

--- src/com/trc/service/AccountService.java
+++ src/com/trc/service/AccountService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.trc.exception.service.AccountServiceException;
+import com.trc.service.AccountServiceModel;
 import com.trc.service.gateway.TruConnectGateway;
 import com.trc.service.gateway.TruConnectUtil;
 import com.trc.user.User;
