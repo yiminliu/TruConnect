78f55c0cef1ab53d48503ea42eb3e70d29439e97
yiminliu <yimin_l@hotmail.com>
2012-12-14 13:15:00 -0800

    added IN_PROCESS_LOCKED as a new status

--- src_ticketing/com/trc/domain/ticket/TicketStatus.java
+++ src_ticketing/com/trc/domain/ticket/TicketStatus.java
@@ -1,7 +1,7 @@
 package com.trc.domain.ticket;
 
 public enum TicketStatus {
-	OPEN, IN_PROCESS, CLOSED, RESOLVED, ON_HOLD, UNRESOLVED, REJECTED, REOPEN;
+	OPEN, IN_PROCESS, IN_PROCESS_LOCKED, CLOSED, RESOLVED, ON_HOLD, UNRESOLVED, REJECTED, REOPEN;
 	String key;
 	TicketStatus(){}
 	
